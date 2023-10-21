90615873d07c7859b5cb21e1a8542fa2114a2f4b
dasom222g <dasom228@gmail.com>
2023-10-21 19:02:03 +0900

    Add layout style

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Complete from "./pages/Complete";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<Quiz />} />
-      <Route path="/result" element={<Result />} />
-      <Route path="/complete" element={<Complete />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/complete" element={<Complete />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  // logic
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log("pathname", pathname);
+  });
+  // view
+  return (
+    <div
+      className={`h-full py-10 px-6 ${
+        pathname === "/" ? "bg-mb-blue-600" : "bg-mb-gray-800"
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
