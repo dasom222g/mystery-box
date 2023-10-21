2e65b6f3538bf8f7457b4c32c9a1f843f31daed8
dasom222g <dasom228@gmail.com>
2023-10-21 19:01:15 +0900

    Fix style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mb-pink-700": "#FF384A",
+        "mb-blue-600": "#5B4BEA",
+        "mb-blue-700": "#4B5EFC",
+        "mb-gray-800": "#181A20",
+        "mb-gray-700": "#2D3342",
+        "mb-green-400": "#05DB92",
+        "mb-green-200": "#BFFFE9",
+      },
+      borderRadius: {
+        "mb-lg": "0.625rem",
+      },
+    },
   },
   plugins: [],
 };
