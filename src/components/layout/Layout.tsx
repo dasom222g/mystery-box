26e6c35177e3e17e2b554c12f2deff90c763e613
dasom222g <dasom228@gmail.com>
2023-10-28 18:34:20 +0900

    Fix style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   // view
   return (
     <div
-      className={`h-full py-10 px-5 ${
+      className={`h-full py-10 px-5 overflow-auto ${
         pathname === "/" ? "bg-mb-blue-600" : "bg-mb-gray-800"
       }`}
     >
