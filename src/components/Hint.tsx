7c3ec0994f2eaca1b80cd02f92a4329ecffe7f7f
dasom222g <dasom228@gmail.com>
2023-10-28 18:36:39 +0900

    Fix logic

--- src/components/Hint.tsx
+++ src/components/Hint.tsx
@@ -4,11 +4,12 @@ import { HintValueType } from "../lib/type";
 import { initialHintValue } from "../data/initialState";
 
 interface HintProps {
+  id: number;
   answer: number | null;
-  onSubmit: (value: HintValueType) => void;
+  onSubmit: (value: HintValueType, id: number) => void;
 }
 
-const Hint: FC<HintProps> = ({ answer, onSubmit }) => {
+const Hint: FC<HintProps> = ({ id, answer, onSubmit }) => {
   // logic
   const [value, setValue] = useState<HintValueType>(initialHintValue);
   const { left, right } = value;
@@ -16,7 +17,7 @@ const Hint: FC<HintProps> = ({ answer, onSubmit }) => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    onSubmit(value);
+    onSubmit(value, id);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,44 +32,46 @@ const Hint: FC<HintProps> = ({ answer, onSubmit }) => {
   // view
   return (
     <form
-      id="hintForm"
-      className="flex items-center w-full h-12 text-white text-xl"
+      id={`hintForm${id}`}
+      className="w-full text-white text-xl py-3"
       onSubmit={handleSubmit}
     >
-      <input
-        type="number"
-        name="left"
-        value={left}
-        required={true}
-        disabled={disabled}
-        className="w-16 py-2 px-2 border border-white rounded-mb-lg bg-transparent disabled:bg-mb-gray-700 disabled:border-0"
-        onChange={handleChange}
-      />
-      <div className="px-2">
-        <Box />
-      </div>
-      <input
-        type="text"
-        name="right"
-        value={right}
-        required={true}
-        disabled={disabled}
-        className="w-16 py-2 px-2 border border-white rounded-mb-lg bg-transparent disabled:bg-mb-gray-700 disabled:border-0"
-        onChange={handleChange}
-      />
-      <div className={disabled ? "" : "px-4"}>
-        <button
-          type="submit"
-          form="hintForm"
+      <div className="flex items-center h-12 w-full">
+        <input
+          type="number"
+          name="left"
+          value={left}
+          required={true}
           disabled={disabled}
-          className="w-12 p-3 bg-mb-blue-700 rounded-mb-lg disabled:bg-transparent"
-        >
-          =
-        </button>
+          className="w-16 py-2 px-2 border border-white rounded-mb-lg bg-transparent disabled:bg-mb-gray-700 disabled:border-0"
+          onChange={handleChange}
+        />
+        <div className="px-2">
+          <Box />
+        </div>
+        <input
+          type="text"
+          name="right"
+          value={right}
+          required={true}
+          disabled={disabled}
+          className="w-16 py-2 px-2 border border-white rounded-mb-lg bg-transparent disabled:bg-mb-gray-700 disabled:border-0"
+          onChange={handleChange}
+        />
+        <div className={disabled ? "" : "px-4"}>
+          <button
+            type="submit"
+            form={`hintForm${id}`}
+            disabled={disabled}
+            className="w-12 p-3 bg-mb-blue-700 rounded-mb-lg disabled:bg-transparent"
+          >
+            =
+          </button>
+        </div>
+        {/* START: 정답부분 */}
+        {answer !== null && <span>{answer}</span>}
+        {/* START: 정답부분 */}
       </div>
-      {/* START: 정답부분 */}
-      {answer !== null && <span>{answer}</span>}
-      {/* START: 정답부분 */}
     </form>
   );
 };
